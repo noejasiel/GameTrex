178a5cd5820cb6e2627b4e072da7d941c70c1a7d
noejasiel <55604384+noejasiel@users.noreply.github.com>
2020-09-12 19:49:11 -0500

    falta pulir

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 var canvas = document.getElementById("canvas");
 var cont_canvas = canvas.getContext("2d");
 
+// mi pantalla de perder
+var gameOver = document.getElementById("retry");
+
+// mi btn de retry
+var retry = document.getElementById("btn_retry");
+retry.addEventListener("click", function () {
+  location.reload();
+});
+
 var dino_src = "./assets/trex.png";
 var cactus_src = "./assets/obstacle_alone.png";
 var piso_src = "./assets/piso.png";
@@ -11,9 +20,34 @@ var sol_src = "./assets/solB.png";
 var sol_src2 = "./assets/luna.png";
 var pajaro_src = "./assets/pajaro.png";
 
+// objetos
+
 var pajaros = {
   movX: 790,
 };
+
+var nube_mov = {
+  movX1: 400,
+  movX2: 700,
+  movX3: 100,
+};
+var cactus_mov = {
+  y: 260,
+  x: 270,
+  movX: 2,
+};
+
+var cactus_mov2 = {
+  y: 260,
+  x: 691,
+  movX: 2,
+};
+var trex = {
+  x: 100,
+  y: 239,
+  saltando: false,
+};
+
 var pajaro = new Image();
 pajaro.src = pajaro_src;
 pajaro.addEventListener("load", dibujarPajaro);
@@ -42,7 +76,8 @@ function dibujarSol() {
     canvas.style.background = "#020E30";
     cont_canvas.drawImage(sol2, sol_mov, 50);
   } else {
-    canvas.style.background = "#0000";
+    // canvas.style.background = "#0000";
+    canvas.style.background = "#020E30";
     cont_canvas.drawImage(sol, sol_mov, 50);
   }
 }
@@ -56,12 +91,6 @@ function movSol() {
 var nube = new Image();
 nube.src = cloud_src;
 nube.addEventListener("load", dibujarNube);
-
-var nube_mov = {
-  movX1: 400,
-  movX2: 700,
-  movX3: 100,
-};
 
 function dibujarNube() {
   cont_canvas.drawImage(nube, nube_mov.movX1, 107, 64, 23);
@@ -110,18 +139,6 @@ function dibujarCactus() {
   cont_canvas.drawImage(cactus, cactus_mov.x, cactus_mov.y, 20, 25);
 }
 
-var cactus_mov = {
-  y: 260,
-  x: 270,
-  movX: 2,
-};
-
-var cactus_mov2 = {
-  y: 260,
-  x: 691,
-  movX: 2,
-};
-
 function movCactus() {
   cactus_mov.x -= cactus_mov.movX;
   if (cactus_mov.x <= -128) {
@@ -130,25 +147,52 @@ function movCactus() {
   cont_canvas.fillStyle = "#ff2626"; // Color rojo
 
   cont_canvas.beginPath(); // Iniciar trazo
-  cont_canvas.arc(100 + 45, 240 + 43, 1, 0, Math.PI * 2, true); // Dibujar un punto usando la funcion arc
-  cont_canvas.arc(
-    cactus_mov.x + 20,
-    cactus_mov.y + 23,
-    1,
-    0,
-    Math.PI * 2,
-    true
-  );
+  cont_canvas.arc(trex.x + 50, trex.y + 30, 1, 0, Math.PI * 2, true); // Dibujar un punto usando la funcion arc
+  cont_canvas.arc(cactus_mov.x, cactus_mov.y + 10, 1, 0, Math.PI * 2, true);
   cont_canvas.fill();
 }
 function Colision() {
-  console.log(
-    trex.x,
-    trex.y,
-    cactus_mov.x,
-    cactus_mov.y,
-    "velocidad" + cactus_mov.movX
-  );
+  if (cactus_mov.x < 160 && cactus_mov.x > 140) {
+    console.log(
+      trex.x + 50,
+      // trex.y + 31,
+      cactus_mov.x,
+      // cactus_mov.y + 10,
+      "velocidad" + cactus_mov.movX
+    );
+  }
+
+  // es la parte donde hace colion el dino con el cactus
+  // vel 2  y 6
+  if (
+    cactus_mov.y + 10 == trex.y + 31 &&
+    cactus_mov.x < 153 &&
+    cactus_mov.x > 150
+  ) {
+    colision_canvas = true;
+    console.log("colision");
+  }
+  // vel 4
+  else if (
+    cactus_mov.y + 10 == trex.y + 31 &&
+    cactus_mov.x < 155 &&
+    cactus_mov.x > 150 &&
+    cactus_mov.movX === 4
+  ) {
+    colision_canvas = true;
+    console.log("colision 4");
+  }
+
+  // vel 8
+  else if (
+    cactus_mov.y + 10 == trex.y + 31 &&
+    cactus_mov.x < 154 &&
+    cactus_mov.x > 152 &&
+    cactus_mov.movX === 8
+  ) {
+    colision_canvas = true;
+    console.log("colison 8");
+  }
 }
 
 function movCactus2() {
@@ -158,11 +202,6 @@ function movCactus2() {
   }
 }
 
-var trex = {
-  x: 100,
-  y: 239,
-  saltando: false,
-};
 function dibujarDino() {
   if (dino_Abajo === true) {
     cont_canvas.drawImage(Dino_down, trex.x, trex.y, 45, 45);
@@ -174,7 +213,7 @@ function dibujarPiso() {
   cont_canvas.drawImage(piso, 0, 280, width + 15, 10);
 }
 document.addEventListener("keyup", function (event) {
-  if (event.keyCode == 32) {
+  if (event.keyCode == 32 || event.keyCode == 38) {
     saltar();
   }
 });
@@ -185,17 +224,24 @@ var score = 0;
 
 var colision_canvas = false;
 function borrarCanvas() {
-  // if (colision_canvas == true) {
-  //   canvas.width = 0;
-  //   canvas.heigth = 0;
-  //   //fin del game
-  //   document.getElementById("canvas").classList.remove("canvas");
-  // } else {
-  canvas.width = width;
-  canvas.heigth = heigth;
-  score += 1;
-  // console.log(score, "score");
-  // }
+  if (colision_canvas == true) {
+    canvas.width = 0;
+    canvas.heigth = 0;
+    //fin del game
+    document.getElementById("canvas").style.display = "none";
+    gameOver.style.display = "inherit";
+  } else {
+    canvas.width = width;
+    canvas.heigth = heigth;
+    var conten = document.getElementById("conteiner-score");
+    conten.innerHTML = `
+        <p>score</p>
+        <p > ${score}</p>
+    `;
+
+    score += 1;
+    // console.log(score, "score");
+  }
 }
 
 function saltar() {
@@ -265,6 +311,11 @@ function scoreFun() {
 var FPS = 50;
 
 setInterval(function () {
+  principal();
+  // dibujarDinoDown();
+}, 1000 / FPS);
+
+function principal() {
   borrarCanvas();
   dibujarPiso();
   dibujarDino();
@@ -281,6 +332,4 @@ setInterval(function () {
   movSol();
   movPajaro();
   Colision();
-
-  // dibujarDinoDown();
-}, 1000 / FPS);
+}
